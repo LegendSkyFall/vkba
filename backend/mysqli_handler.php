e3febbf64e8d3e079f6839ef2b8a93a892aac74f
LegendSkyFall <giuseppecorso@outlook.de>
2016-03-18 22:28:24 +0100

    Create mysqli_handler.php
    Erster Code zur Umstruktierung von bestehendem Code (Allgemeine Funktionen + Klassen, welche sich um DB-Funktionen kümmern sollen).

---
+++ backend/mysqli_handler.php
@@ -0,0 +1,15 @@
+<?php
+/*
+ *Backend-Teil, welcher zukünftig für alle DB-Funktionen verwendet wird
+ */
+ 
+ class Database {
+ 
+  //Datenbank-Konfiguration
+  private $db_host = "localhost";
+  private $db_user = "username";
+  private $db_pass = "password";
+  private $db_name = "database";
+  
+ }
+?>
