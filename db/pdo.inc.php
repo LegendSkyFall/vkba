23a6bde657d285ab1db35eab804d12faf86291f9
LegendSkyFall <giuseppecorso@outlook.de>
2016-04-07 20:43:46 +0200

    implemented debug variable
    will be used later to toggle debug mode

--- db/pdo.inc.php
+++ db/pdo.inc.php
@@ -4,4 +4,11 @@ define('MYSQL_HOST', 'localhost');
 define('MYSQL_USER', 'db_user');
 define('MYSQL_PASS', 'db_pass');
 define('MYSQL_DB', 'db_name');
+
+/*
+debugging variable
+only for testing purposes,
+can contain sensible data
+*/
+define('DEBUG', true);
 ?>
