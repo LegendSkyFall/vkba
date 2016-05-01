71dac0c85c3e85f8f11e6ce432a1fdd82c6d7a91
LegendSkyFall <support@legendskyfall.de>
2016-05-01 12:43:23 +0200

    add transactions overview layout

--- index.php
+++ index.php
@@ -8,6 +8,7 @@
     include('include/usernav.php');
     ?>
     <script>
+    /* will be used later so that page doesn't have to reload */
     /* function for deleting servermessages */
     function delMessage() {
       $.post("include/backend/sysMessage.php", $("#sysMessageForm").serialize())
@@ -54,6 +55,20 @@
                   ?>
                 </span>
                 Kontostand in Kadis
+              </div><!-- sm-st-info -->
+            </div><!-- sm-st clearfix -->
+          </div><!-- col-md-3 -->
+          <!-- user transaction number -->
+          <div class="col-md-3">
+            <div class="sm-st clearfix">
+              <span class="sm-st-icon st-red"><i class="fa fa-exchange"></i></span>
+              <div class="sm-st-info">
+                <span>
+                  <?php
+                  echo $getNumTransactions;
+                  ?>
+                </span>
+                Anzahl der Transaktionen
               </div><!-- sm-st-info -->
             </div><!-- sm-st clearfix -->
           </div><!-- col-md-3 -->
