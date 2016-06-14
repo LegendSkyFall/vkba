b6a5d7dcd29b719ad97b97790ddc8ab52c929902
LegendSkyFall <support@legendskyfall.de>
2016-06-14 20:47:45 +0200

    fixed pdo fatal error, undefined method

--- db/pdo.inc.php
+++ db/pdo.inc.php
@@ -20,7 +20,7 @@ class DB extends PDO{
           PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES utf8",
           PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION
         ];
-        parent::_construct('mysql_host=' . MYSQL_HOST . ';dbname=' . MYSQL_DB . ';charset=utf8', MYSQL_USER, MYSQL_PASS, $options);
+        parent::__construct('mysql_host=' . MYSQL_HOST . ';dbname=' . MYSQL_DB . ';charset=utf8', MYSQL_USER, MYSQL_PASS, $options);
         $this->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
         $this->setAttribute(PDO::ATTR_EMULATE_PREPARES, false);
     }catch(Exception $e){
