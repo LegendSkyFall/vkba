7f29646f4f9d81bf1b492e7f653ae81117831418
LegendSkyFall <support@legendskyfall.de>
2016-06-16 21:43:23 +0200

    created include folder with head file

---
+++ include/head.php
@@ -0,0 +1,20 @@
+<head>
+  <meta charset="utf-8">
+  <title>VKBA Rewrite</title>
+  <!-- optimization for mobile phones and small screens -->
+  <meta content='width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no' name='viewport'>
+  <meta name="description" content="Developed By LegendSkyFall">
+  <meta name="keywords" content="Virtual Kadcon Bank Accounts, LEGEND-BANK, LegendSkyFall, VKBA, Kadcon">
+  <!-- bootstrap v.3.0.2 -->
+  <link href="style/bootstrap.min.css" rel="stylesheet" type="text/css" />
+  <!-- font Awesome CDN-->
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.5.0/css/font-awesome.min.css">
+  <!-- Morris chart -->
+  <link href="style/morris/morris.css" rel="stylesheet" type="text/css" />
+  <!-- iCheck for checkboxes and radio inputs -->
+  <link href="style/iCheck/all.css" rel="stylesheet" type="text/css" />
+  <!-- Lato font CDN -->
+  <link href='https://fonts.googleapis.com/css?family=Lato' rel='stylesheet' type='text/css'>
+  <!-- theme style -->
+  <link href="style/style.css" rel="stylesheet" type="text/css" />
+</head>
