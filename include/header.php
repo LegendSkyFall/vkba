2f6596cbdcd117da545d18a05bbb43db7598764c
LegendSkyFall <support@legendskyfall.de>
2016-06-17 20:21:32 +0200

    added code for header, implemented layout for transactions

---
+++ include/header.php
@@ -0,0 +1,40 @@
+<header class="header">
+  <a href="index.php" class="logo">
+    VKBA Rewrite
+  </a>
+  <nav class="navbar navbar-static-top" role="navigation">
+    <!-- sidebar toggle button -->
+    <a href="#" class="navbar-btn sidebar-toggle" data-toggle="offcanvas" role="button">
+      <span class="sr-only">Toggle navigation</span>
+      <span class="icon-bar"></span>
+      <span class="icon-bar"></span>
+      <span class="icon-bar"></span>
+    </a>
+    <div class="navbar-right">
+      <ul class="nav navbar-nav">
+        <li class="dropdown messages-menu">
+          <a href="#" class="dropdown-toggle" data-toggle="dropdown">
+            <i class="fa fa-inbox"></i>
+            <span class="label label-primary">0</span>
+          </a>
+          <ul class="dropdown-menu">
+            <li class="header">0 neue Transaktionen</li>
+              <li>
+                <!-- will contain unread transactions in the future -->
+                <ul class="menu">
+                  <li>
+                    <a href="#">
+                      <div class="pull-left">
+                        <img src="" class="img-circle" alt="user image" />
+                      </div>
+                      <h4>Benutzer</h4>
+                      <p>Derzeit noch nicht verfügbar.</p>
+                      <small class="pull-right"><i class="fa fa-clock-o"></i> 00:00</small>
+                    </a>
+                  </li>
+                </ul><!-- end menu -->
+              </li>
+              <li class="footer"><a href="#">Alle Transaktionen einsehen</a></li>
+            </ul><!-- end dropdown-menu -->
+          </li><!-- end dropdown messages-menu -->
+</header>
