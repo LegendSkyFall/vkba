f95e159be92676c7a1a8793cde12a4ee360d32fe
LegendSkyFall <support@legendskyfall.de>
2016-08-08 21:06:29 +0200

    implemented AddOn Management

--- include/header.php
+++ include/header.php
@@ -1,3 +1,129 @@
+<?php
+# handle buyAddOn
+if(isset($_POST["buyAddOn"])){
+  # CSRF-Protection
+  if($_POST["token"] != $_SESSION["csrf_token"]){
+    exit("Illegaler Zugriffsversuch!");
+  }
+  # error handling variable
+  $error = false;
+  # check if submitted addOnID is valid
+  $availableAddOns = array(1, 2);
+  if(!in_array($_POST["addOnID"], $availableAddOns)){
+    # unknown addOn
+    $error = true;
+    $errorMessage = "Dieses AddOn existiert nicht.";
+  }
+  # check if user already has AddOn
+  $checkAddOn = $db->prepare("SELECT add_id FROM AddOns WHERE username=:username AND add_id=:add_id");
+  $checkAddOn->bindValue(":username", $_SESSION["user"], PDO::PARAM_STR);
+  $checkAddOn->bindValue(":add_id", $_POST["addOnID"], PDO::PARAM_INT);
+  $checkAddOn->execute();
+  $hasAddOn = ($checkAddOn->rowCount() > 0) ? true : false;
+  if($hasAddOn){
+    # user has AddOn
+    $error = true;
+    $errorMessage = "Du besitzt dieses AddOn bereits.";
+  }
+  # check if AddOn is for specified account type
+  if($_POST["addOnID"] == 2){
+    if($_SESSION["ktype"] != 0){
+      # not for this type
+      $error = true;
+      $errorMessage = "Dieses AddOn ist nicht für Deinen Kontotypen verfügbar.";
+    }
+  }
+  if(!$error){
+    # buy AddOn
+    $buyAddOn = $db->prepare("INSERT INTO AddOns (add_id, username) VALUES (:add_id, :username)");
+    $buyAddOn->bindValue(":add_id", $_POST["addOnID"], PDO::PARAM_INT);
+    $buyAddOn->bindValue(":username", $_SESSION["user"], PDO::PARAM_STR);
+    $buyAddOn->execute();
+    if($buyAddOn){
+      # successfull
+      $successMessage = "AddOn-Kauf war erfolgreich. Viel Spaß!";
+    }
+  }
+}
+# handle deleteAddOn
+if(isset($_POST["deleteAddOn"])){
+  # CSRF-Protection
+  if($_POST["token"] != $_SESSION["csrf_token"]){
+    exit("Illegaler Zugriffsversuch!");
+  }
+  # error handling variable
+  $error = false;
+  # check if submitted addOnID is valid
+  $availableAddOns = array(1, 2);
+  if(!in_array($_POST["addOnID"], $availableAddOns)){
+    # unknown addOn
+    $error = true;
+    $errorMessage = "Dieses AddOn existiert nicht.";
+  }
+  # check if user really has AddOn
+  $checkAddOn = $db->prepare("SELECT add_id FROM AddOns WHERE username=:username AND add_id=:add_id");
+  $checkAddOn->bindValue(":username", $_SESSION["user"], PDO::PARAM_STR);
+  $checkAddOn->bindValue(":add_id", $_POST["addOnID"], PDO::PARAM_INT);
+  $checkAddOn->execute();
+  $hasAddOn = ($checkAddOn->rowCount() > 0) ? true : false;
+  if(!$hasAddOn){
+    # user doesn't have AddOn
+    $error = true;
+    $errorMessage = "Du besitzt dieses AddOn gar nicht.";
+  }
+  # check if AddOn is for specified account type
+  if($_POST["addOnID"] == 2){
+    if($_SESSION["ktype"] != 0){
+      # not for this type
+      $error = true;
+      $errorMessage = "Dieses AddOn ist nicht für Deinen Kontotypen verfügbar.";
+    }
+  }
+  # calculate termination fees
+  if($_POST["addOnID"] == 1){
+    $terminationFees = (30/100) * 75;
+  }elseif($_POST["addOnID"] == 2){
+    $terminationFees = (65/100) * 75;
+  }
+  # get actual balance
+  $getUserBalance = $db->prepare("SELECT balance FROM Accounts WHERE username=:username");
+  $getUserBalance->bindValue(":username", $_SESSION["user"], PDO::PARAM_STR);
+  $getUserBalance->execute();
+  foreach($getUserBalance as $balance){
+    $userBalance = $balance["balance"];
+  }
+  if(empty($terminationFees)){
+    # wrong AddOn
+    $error = true;
+  }else{
+    if($userBalance < $terminationFees){
+      # not enough money for termination
+      $error = true;
+      $errorMessage = "Du hast nicht genügend Geld, um dieses AddOn zu kündigen.";
+    }else{
+      # calculate new balance
+      $newUserBalance = round($userBalance - $terminationFees, 2);
+    }
+  }
+  # check if user has enough money for termination
+  if(!$error){
+    # update user balance
+    $updateUserBalance = $db->prepare("UPDATE Accounts SET balance=:balance WHERE username=:username");
+    $updateUserBalance->bindValue(":balance", $newUserBalance, PDO::PARAM_STR);
+    $updateUserBalance->bindValue(":username", $_SESSION["user"], PDO::PARAM_STR);
+    $updateUserBalance->execute();
+    # delete AddOn
+    $deleteAddOn = $db->prepare("DELETE FROM AddOns WHERE username=:username AND add_id=:add_id");
+    $deleteAddOn->bindValue(":username", $_SESSION["user"], PDO::PARAM_STR);
+    $deleteAddOn->bindValue(":add_id", $_POST["addOnID"], PDO::PARAM_INT);
+    $deleteAddOn->execute();
+    if($deleteAddOn){
+      # successfull
+      $successMessage = "AddOn-Kündigung war erfolgreich. Du kannst es jederzeit wieder aktivieren!";
+    }
+  }
+}
+?>
 <header class="header">
   <a href="index.php" class="logo">
     VKBA Rewrite
@@ -46,15 +172,15 @@
             <ul class="dropdown-menu dropdown-custom dropdown-menu-right">
               <li class="dropdown-header text-center">VKBA-Account</li>
               <li>
-                <a data-toggle="modal" href="#myModal2">
+                <a data-toggle="modal" href="#modalAddOn">
                   <i class="fa fa-plus fa-fw pull-right"></i>
                   <span class="badge badge-success pull-right">2</span> Add-Ons
                 </a>
-                <a data-toggle="modal" href="#myModal3">
+                <a data-toggle="modal" href="#modalContacts">
                   <i class="fa fa-users fa-fw pull-right"></i>
                   <span class="badge badge-danger pull-right">0</span> Kontakte
                 </a>
-                <a data-toggle="modal" href="#myModal3">
+                <a data-toggle="modal" href="#modalSettings">
                   <i class="fa fa-cog fa-fw pull-right"></i>
                   VKBA-Einstellungen
                 </a>
@@ -71,7 +197,7 @@
 </header>
 
 <!-- Modal AddOns-->
-<div class="modal fade" id="myModal2" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true">
+<div class="modal fade" id="modalAddOn" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true">
   <div class="modal-dialog">
     <div class="modal-content">
       <div class="modal-header">
@@ -82,20 +208,36 @@
         <form method="post" action="">
           <div class="col-md-12">
             <div class="sm-st clearfix">
-              <?php
-              //TODO
-              ?>
               <div class="sm-st-info">
                 <?php
-                //TODO
+                # get AddOn1
+                $addOnONE = $db->prepare("SELECT id FROM AddOns WHERE username=:username AND add_id=1");
+                $addOnONE->bindValue(":username", $_SESSION["user"], PDO::PARAM_STR);
+                $addOnONE->execute();
+                $hasAddOnONE = ($addOnONE->rowCount() > 0) ? true : false;
+                # check if user has AddOn1
+                if($hasAddOnONE){
+                  # user has AddOn1
+                  echo "<input type='hidden' name='token' value='" . $_SESSION["csrf_token"] . "'>";
+                  echo "<input type='hidden' name='addOnID' value=1>";
+                  echo "<button type='submit' name='deleteAddOn' class='btn btn-default btn-sm pull-right'>";
+                    echo "<span style='color: #DC2E31' class='glyphicon glyphicon-remove' aria-hidden='true'></span>";
+                  echo "</button>";
+                }else{
+                  # user has not AddOn1
+                  echo "<input type='hidden' name='token' value='" . $_SESSION["csrf_token"] . "'>";
+                  echo "<input type='hidden' name='addOnID' value=1>";
+                  echo "<button type='submit' name='buyAddOn' class='btn btn-default btn-sm pull-right'>";
+                    echo "<span style='color: #088A08' class='glyphicon glyphicon-plus' aria-hidden='true'></span>";
+                  echo "</button>";
+                }
                 ?>
-                <button type="submit" name="deleteAddon" class="btn btn-default btn-sm pull-right">
-                  <span style="color: #DC2E31" class="glyphicon glyphicon-remove" aria-hidden="true"></span>
-                </button>
                 <span>Werbe-AddOn</span>
-                Mit diesem AddOn bist Du berechtigt, Werbung beispielsweise für Deinen Warp auf Kadcon, oder aber auch für VKBA-Announcen zu schalten. Werbung erscheint zufällig bei QuickBuy.
+                Das AddOn ermöglicht Dir das Schalten eigener Werbung, bspw. für Deinen Warp auf Kadcon oder aber auch für eigene QuickBuy-Inserate. Die Werbung wird zufällig auf QuickBuy geschaltet.<br>
+                Sie bleibt eine Woche lang aktiv. Danach kannst Du sie unter Aktionen 'Werbung schalten' wieder reaktivieren oder den Text ändern. Du kannst bis zu drei Werbungen pro Woche schalten.<br>
+                Die erste Werbung pro Woche ist immer gratis, jede weitere kostet Dich einmalig 25 Kadis. Den AddOn-Preis zahlst Du wöchentlich immer gleich, unabhängig davon, wie viele aktiv sind.<br>
                 <br>
-                <b>Kosten:</b> 50 Kadis/Woche
+                <b>Kosten:</b> 30 Kadis/Woche
               </div><!-- end sm-st-info -->
             </div><!-- end sm-st clearfix -->
           </div><!-- end col-md-12 -->
@@ -103,18 +245,32 @@
         <form method="post" action="">
           <div class="col-md-12">
             <div class="sm-st clearfix">
-              <?php
-              //TODO
-              ?>
               <div class="sm-st-info">
                 <?php
-                //TODO
+                # get AddOn1
+                $addOnTWO = $db->prepare("SELECT id FROM AddOns WHERE username=:username AND add_id=2");
+                $addOnTWO->bindValue(":username", $_SESSION["user"], PDO::PARAM_STR);
+                $addOnTWO->execute();
+                $hasAddOnTWO = ($addOnTWO->rowCount() > 0) ? true : false;
+                # check if user has AddOn1
+                if($hasAddOnTWO){
+                  # user has AddOn1
+                  echo "<input type='hidden' name='token' value='" . $_SESSION["csrf_token"] . "'>";
+                  echo "<input type='hidden' name='addOnID' value=2>";
+                  echo "<button type='submit' name='deleteAddOn' class='btn btn-default btn-sm pull-right'>";
+                    echo "<span style='color: #DC2E31' class='glyphicon glyphicon-remove' aria-hidden='true'></span>";
+                  echo "</button>";
+                }else{
+                  # user has not AddOn1
+                  echo "<input type='hidden' name='token' value='" . $_SESSION["csrf_token"] . "'>";
+                  echo "<input type='hidden' name='addOnID' value=2>";
+                  echo "<button type='submit' name='buyAddOn' class='btn btn-default btn-sm pull-right'>";
+                    echo "<span style='color: #088A08' class='glyphicon glyphicon-plus' aria-hidden='true'></span>";
+                  echo "</button>";
+                }
                 ?>
-                <button type="submit" name="deleteAddon" class="btn btn-default btn-sm pull-right">
-                  <span style="color: #DC2E31" class="glyphicon glyphicon-remove" aria-hidden="true"></span>
-                </button>
                 <span>Händler-AddOn</span>
-                Mit diesem AddOn bist Du berechtigt, eigene Inserate bei QuickBuy schalten zu können, auch wenn Du kein Händlerkonto besitzt.<b>AddOn nur für Girkonten verfügbar</b>
+                Dieses AddOn ermöglicht das Erstellen eigener Inserate in QuickBuy für <b>Girokonten</b>.
                 <br>
                 <b>Kosten:</b> 65 Kadis/Woche
               </div><!-- end sm-st-info -->
@@ -126,6 +282,7 @@
           AddOns können jederzeit gekündigt werden.
           Um Missbrauch zu vermeiden, wird bei der Kündigung ein einmaliger Betrag in Höhe der wöchentlichen Kosten zuzüglich einer kleinen Gebühr in Rechnung gestellt.
           Die wöchentlichen AddOn-Kosten werden - unabhängig vom Kaufdatum - immer sonntags automatisch abgebucht. AddOns können jederzeit wieder gekauft werden.
+          Hat der Spieler nicht genügend Geld, die laufenden AddOn-Kosten zu tragen, so wird das AddOn automatisch gekündigt und eine Gebühr in Rechnung gestellt, welche automatisch abgebucht wird.
         </span>
       </div><!-- end modal-body -->
     </div><!-- end modal-content -->
@@ -133,7 +290,7 @@
 </div><!-- end modal -->
 
 <!-- Modal contacts -->
-<div class="modal fade" id="myModal3" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true">
+<div class="modal fade" id="modalContacts" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true">
   <div class="modal-dialog">
     <div class="modal-content">
       <div class="modal-header">
@@ -148,7 +305,7 @@
 </div><!-- end modal -->
 
 <!-- Modal VKBA settings -->
-<div class="modal fade" id="myModal4" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true">
+<div class="modal fade" id="modalSettings" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true">
   <div class="modal-dialog">
     <div class="modal-content">
       <div class="modal-header">
