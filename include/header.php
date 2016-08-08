8aa2d35be9bce0a3dc6d190dee18ec6e2088ddc5
LegendSkyFall <support@legendskyfall.de>
2016-08-09 00:26:38 +0200

    first try bugfixing #67

--- include/header.php
+++ include/header.php
@@ -195,7 +195,59 @@ if(isset($_POST["deleteAddOn"])){
       </div><!-- end navbar-right -->
     </nav><!-- end navbar navbar-static-top -->
 </header>
+<script>
+$(function() {
+    "use strict";
 
+    //Enable sidebar toggle
+    $("[data-toggle='offcanvas']").click(function(e) {
+        e.preventDefault();
+
+        //If window is small enough, enable sidebar push menu
+        if ($(window).width() <= 992) {
+            $('.row-offcanvas').toggleClass('active');
+            $('.left-side').removeClass("collapse-left");
+            $(".right-side").removeClass("strech");
+            $('.row-offcanvas').toggleClass("relative");
+        } else {
+            //Else, enable content streching
+            $('.left-side').toggleClass("collapse-left");
+            $(".right-side").toggleClass("strech");
+        }
+    });
+
+    //Add hover support for touch devices
+    $('.btn').bind('touchstart', function() {
+        $(this).addClass('hover');
+    }).bind('touchend', function() {
+        $(this).removeClass('hover');
+    });
+
+    //Activate tooltips
+    $("[data-toggle='tooltip']").tooltip();
+
+    /*
+     * Add collapse and remove events to boxes
+     */
+    $("[data-widget='collapse']").click(function() {
+        //Find the box parent
+        var box = $(this).parents(".box").first();
+        //Find the body and the footer
+        var bf = box.find(".box-body, .box-footer");
+        if (!box.hasClass("collapsed-box")) {
+            box.addClass("collapsed-box");
+            //Convert minus into plus
+            $(this).children(".fa-minus").removeClass("fa-minus").addClass("fa-plus");
+            bf.slideUp();
+        } else {
+            box.removeClass("collapsed-box");
+            //Convert plus into minus
+            $(this).children(".fa-plus").removeClass("fa-plus").addClass("fa-minus");
+            bf.slideDown();
+        }
+    });
+  });
+</script>
 <!-- Modal AddOns-->
 <div class="modal fade" id="modalAddOn" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true">
   <div class="modal-dialog">
